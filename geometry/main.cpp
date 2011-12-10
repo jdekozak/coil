8d758ef9e9e85fe2ec8f6f179512d5121c3b9ba5
jdekozak <jdekozak@hotmail.com>
2011-12-10 15:31:45 +0100

    More geometries and combinations - More flexibility

--- geometry/main.cpp
+++ geometry/main.cpp
@@ -2,15 +2,125 @@
 #include <iostream>
 
 #include <boost/program_options.hpp>
+#include <boost/function.hpp>
 
-double get_intensity(const double& radius) {
-  return 1;//-std::exp(-1/radius/radius);
-}
+//HEIGHT AND INTENSITY TAGS
+struct constant_tag;
+struct nef_tag;
+struct linear_decrease_tag;
+//INTENSITY
+template<typename PARAMETER=constant_tag>
+struct get_intensity {
+  double operator()(const double&) const {
+    return 1;
+  }
+};
+template<>
+struct get_intensity<linear_decrease_tag> {
+  double operator()(const double& radius) const {
+    return 1-0.01*radius;
+  }
+};
+//INTENSITY INTERFACE
+typedef boost::function<double(const double &)> intensity_t;
 
-double get_height(const double& radius) {
-  return 1-std::exp(-1/radius/radius);
-}
+//HEIGHT
+template<typename PARAMETER=constant_tag>
+struct get_height {
+  double operator()(const double&) const {
+    return 1;
+  }
+};
+template<>
+struct get_height<nef_tag> {
+  double operator()(const double& radius) const {
+    return 1-std::exp(-1/radius/radius);
+  }
+};
+//HEIGHT INTERFACE
+typedef boost::function<double(const double &)> height_t;
 
+//PROFILE TAGS
+struct wall_confinement_tag;
+struct regular_coil_tag;
+//PROFILE
+template<typename PARAMETER=regular_coil_tag>
+struct get_profile {
+  void operator()(const double& radius, const size_t&, const height_t& height, const intensity_t& intensity) const {
+    double z = height(radius);
+    double i = intensity(radius);
+    std::cout << radius << ",0.," << z << "," << i << std::endl;
+    std::cout << radius << ",0.," << -z << "," << i << std::endl;  
+  }
+};
+template<>
+struct get_profile<wall_confinement_tag> {
+  void operator()(const double& radius, const size_t& index, const height_t& height, const intensity_t& intensity) const {
+    double z = height(radius);
+    double i = intensity(radius);
+    if(index % 2) {
+      std::cout << radius << ",0.," << z << "," << i << std::endl;
+      std::cout << radius << ",0.," << -z << "," << i << std::endl;
+    } else {
+      std::cout << radius << ",0.," << z << "," << -i << std::endl;
+      std::cout << radius << ",0.," << -z << "," << -i << std::endl;
+    }
+  }
+};
+//PROFILE INTERFACE
+typedef boost::function<void(const double&, const size_t&, const height_t&, const intensity_t&)> profile_t;
+
+//EXTREMITY TAGS
+struct close_extremity_tag;
+struct open_extremity_tag;
+//EXTREMITY
+template<typename PARAMETER=close_extremity_tag, typename PROFILE=regular_coil_tag>
+struct get_extremity {
+  void operator()(const double& radius, const size_t&, const intensity_t& intensity) const {
+    double i = intensity(radius);
+    std::cout << radius << ",0.,0," << i << std::endl;
+  }
+};
+template<>
+struct get_extremity<close_extremity_tag, wall_confinement_tag> {
+  void operator()(const double& radius, const size_t& index, const intensity_t& intensity) const {
+    double i = intensity(radius);
+    if(index % 2) {
+      std::cout << radius << ",0.,0," << i << std::endl;
+    } else {
+      std::cout << radius << ",0.,0," << -i << std::endl;
+    }
+  }
+};
+template<typename PROFILE>
+struct get_extremity<open_extremity_tag, PROFILE> {
+  void operator()(const double&, const size_t&, const intensity_t&) const {
+    //empty
+  }
+};
+//EXTREMITY INTERFACE
+typedef boost::function<void(const double&, const size_t&, const intensity_t&)> extremity_t;
+
+//APPLICATION
+struct generate_geometry {
+  generate_geometry(const size_t& maximum_number_loops, const double& maximum_radius) :
+    _maximum_number_loops(maximum_number_loops),
+    _step(maximum_radius/maximum_number_loops)
+  {}
+  void operator()(const profile_t& profile, const extremity_t& extremity, const height_t& height, const intensity_t& intensity) const {
+    size_t index;
+    double radius;
+    std::cout << "R,y,Z,I" << std::endl;
+    for(index = 1, radius = _step; index < _maximum_number_loops; ++index, radius+=_step) {
+      profile(radius, index, height, intensity);
+    }
+    extremity(radius, index, intensity);
+  }
+  size_t _maximum_number_loops;
+  double _step;
+};
+
+//OS PLUGIN ;)
 int main(int argc, char* argv[]) {
   size_t maximum_number_loops;
   double maximum_radius;
@@ -19,39 +129,54 @@ int main(int argc, char* argv[]) {
     ("help", "produce help message")
     ("radius", boost::program_options::value<double>(&maximum_radius)->default_value(6.), "maximum radius")
     ("loops", boost::program_options::value<size_t>(&maximum_number_loops)->default_value(13), "number of current loops")
+    ("nef_profile", "use the nef profile for height")
+    ("constant_intensity", "use the same intensity in all loops")
+    ("wall_confinement", "alternate sign of intensity between loops")
+    ("open_extremity", "no loop at z=0")
     ;
 
   boost::program_options::variables_map vm;
   boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
   boost::program_options::notify(vm);
-
   if (vm.count("help")) {
     std::cout << desc << std::endl;
     return 1;
   }
 
-  double step = maximum_radius / maximum_number_loops;
-  size_t index;
-  double radius;
-  double intensity;
-  double height;
-  std::cout << "R,y,Z,I" << std::endl;
-  for(index = 1, radius = step; index < maximum_number_loops; ++index, radius+=step) {
-    height = get_height(radius);
-    intensity = get_intensity(radius);
-    if(index % 2) {
-      std::cout << radius << ",0.," << height << "," << intensity << std::endl;
-      std::cout << radius << ",0.," << -height << "," << intensity << std::endl;
+  generate_geometry application(maximum_number_loops, maximum_radius);
+
+  height_t Z;
+  intensity_t I;
+  profile_t P;
+  extremity_t E;
+
+  if (vm.count("nef_profile")) {
+    Z = get_height<nef_tag>();
+  } else {
+    Z = get_height<constant_tag>();
+  }
+  if (vm.count("constant_intensity")) {
+    I = get_intensity<constant_tag>();
+  } else {
+    I = get_intensity<linear_decrease_tag>();
+  }
+  if (vm.count("wall_confinement")) {
+    P = get_profile<wall_confinement_tag>();
+    if (vm.count("open_extremity")) {
+      E = get_extremity<open_extremity_tag, wall_confinement_tag>();
     } else {
-      std::cout << radius << ",0.," << height << "," << intensity << std::endl;
-      std::cout << radius << ",0.," << -height << "," << intensity << std::endl;
+      E = get_extremity<close_extremity_tag, wall_confinement_tag>();
+    }
+  } else {
+    P = get_profile<regular_coil_tag>();
+    if (vm.count("open_extremity")) {
+      E = get_extremity<open_extremity_tag, regular_coil_tag>();
+    } else {
+      E = get_extremity<close_extremity_tag, regular_coil_tag>();
     }
   }
-  if(index % 2) {
-    std::cout << radius << ",0.,0," << intensity << std::endl;
-  } else {
-    std::cout << radius << ",0.,0," << intensity << std::endl;
-  }
+
+  application(P, E, Z, I);
 
   return 0;
 }
--- glue.py
+++ glue.py
@@ -4,7 +4,7 @@ import subprocess,os
 
 working_directory=os.getcwd()
 
-if True:
+if False:
     loops=open(working_directory+'/loops.csv','w')
     command=[working_directory+'/geometry/geometry_generator', '--radius', '6', '--loops', '113']
     print command
@@ -12,7 +12,7 @@ if True:
     loops.close()
 
     mesh=open(working_directory+'/mesh.csv','w')
-    command=[working_directory+'/mesh/mesh_generator', '--xmax', '9', '--xdiv', '111', '--zdiv', '111', '--zmax', '2']
+    command=[working_directory+'/mesh/mesh_generator', '--xmax', '20', '--xdiv', '111', '--zdiv', '111', '--zmax', '20']
     print command
     subprocess.check_call(command,stdout=mesh)
     mesh.close()
@@ -26,41 +26,50 @@ field=working_directory+'/field.csv'
 geometry=working_directory+'/loops.csv'
 from paraview.simple import *
 
-reader=OpenDataFile(field)
-background=OpenDataFile(geometry)
+if False:
+    background=OpenDataFile(geometry)
 
+    background_filter=TableToPoints(background)
+    background_filter.XColumn='R'
+    background_filter.YColumn='y'
+    background_filter.ZColumn='Z'
+    background_filter.UpdatePipeline()
+
+reader=OpenDataFile(field)
 filter=TableToPoints(reader)
 filter.XColumn='x'
 filter.YColumn='y'
 filter.ZColumn='z'
 filter.UpdatePipeline()
 
-background_filter=TableToPoints(background)
-background_filter.XColumn='R'
-background_filter.YColumn='y'
-background_filter.ZColumn='Z'
-background_filter.UpdatePipeline()
-
 vector=Calculator(filter)
 vector.ResultArrayName='B'
 vector.Function='Bx*iHat+By*jHat+Bz*kHat'
 vector.UpdatePipeline()
 
-streams=StreamTracer(vector, SeedType="High Resolution Line Source" )
-streams.Vectors = ['POINTS', 'B']
+if True:
+    glyph=Glyph(vector, GlyphType='Arrow')
+    glyph.Vectors = ['POINTS', 'B']
+    glyph.SetScaleFactor = 10000.0
 
-streams.MaximumError = 1.0e-02
-streams.InitialStepLength = 0.1
-streams.MaximumStepLength = 0.4
-streams.MaximumSteps = 20000
-streams.MinimumStepLength = 0.02
-streams.MaximumStreamlineLength = 8.99
-streams.TerminalSpeed = 9.99e-12
 
-streams.SeedType = "High Resolution Line Source"
-streams.SeedType.Point1 = [8.99, 0.0,  0.01]
-streams.SeedType.Point2 = [0.01, 0.0, -0.01]
-streams.SeedType.Resolution = 100
+if False:
+    streams=StreamTracer(vector, SeedType="High Resolution Line Source" )
+    streams.Vectors = ['POINTS', 'B']
+
+    streams.MaximumError = 1.0e-02
+    streams.InitialStepLength = 0.1
+    streams.MaximumStepLength = 0.5
+    streams.MaximumSteps = 20000
+    streams.MinimumStepLength = 0.002
+    streams.MaximumStreamlineLength = 8.99
+    streams.TerminalSpeed = 1e-30
+
+    streams.SeedType = "High Resolution Line Source"
+    streams.SeedType.Point1 = [8.99, 0.0,  0.01]
+    streams.SeedType.Point2 = [0.01, 0.0, -0.01]
+    streams.SeedType.Resolution = 100
+    streams.UpdatePipeline()
 
 Show()
 
@@ -68,14 +77,22 @@ view=Render()
 view.ViewSize=[400,400]
 view.UseLight=1
 view.CameraViewUp=[0.0, 0.0, 1.0]
-view.CameraPosition=[2.9700000000000002, -12.09592162094027, 0.0]
-view.CameraFocalPoint=[2.9700000000000002, 0.0, 0.0]
+view.CameraPosition=[2.97, -12.0959, 0.0]
+view.CameraFocalPoint=[2.97, 0.0, 0.0]
 
 dp=GetDisplayProperties(vector)
 dp.ColorArrayName='B'
-lut = GetLookupTableForArray("B", 3, RGBPoints=[6.0567334430367652e-10, 0.23000000000000001, 0.29899999999999999, 0.754, 3.4436464684981819e-05, 0.70599999999999996, 0.016, 0.14999999999999999], VectorMode='Magnitude', ColorSpace='Diverging', UseLogScale=1, ScalarRangeInitialized=1.0)
+lut = GetLookupTableForArray('B', 3,RGBPoints=[5.5806518400631296e-07, 0.23000000000000001, 0.29899999999999999, 0.754, 8.9029364504078089e-05, 0.70599999999999996, 0.016, 0.14999999999999999] , VectorMode='Magnitude', ColorSpace='Diverging', UseLogScale=1, ScalarRangeInitialized=1.0)
 
 dp.LookupTable=lut
 dp.ColorAttributeType='POINT_DATA'
+
+dp1=GetDisplayProperties(glyph)
+dp1.ColorArrayName='GlyphVector'
+lut1 = GetLookupTableForArray('GlyphVector', 3,RGBPoints=[5.5806518400631296e-07, 0.23000000000000001, 0.29899999999999999, 0.754, 8.9029364504078089e-05, 0.70599999999999996, 0.016, 0.14999999999999999] , VectorMode='Magnitude', ColorSpace='Diverging', UseLogScale=1, ScalarRangeInitialized=1.0)
+dp1.LookupTable=lut1
+dp1.ColorAttributeType='POINT_DATA'
+
+
 view.StillRender()
 
